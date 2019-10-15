d0b1afcd6fc5ed296f1784a03a1b21dcb34bc79f
Fred M <farzad.moghbel@gmail.com>
2019-10-15 14:34:46 -0400

    minor changes on server.js

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const routes = require("./routes");
 const db = require("./models");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // Routes
 app.use(routes);
