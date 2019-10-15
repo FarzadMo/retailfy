11060b68cafbe80ff10b80a01ceb6a2144a9a40a
Fred M <farzad.moghbel@gmail.com>
2019-10-15 12:43:38 -0400

    api routes created-backend

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
 require("dotenv").config();
-var express = require("express");
-var db = require("./models");
+const express = require("express");
+const routes = require("./routes");
+const db = require("./models");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
 // Routes
+app.use(routes);
 
-
-var syncOptions = { force: false };
+let syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-    syncOptions.force = true;
+  syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function () {
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    });
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
-
 
 module.exports = app;
