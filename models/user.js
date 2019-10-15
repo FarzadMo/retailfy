7f239f81a8d74740d747a875efa3ac54e6caa037
soheila <soheilalotfi@ymail.com>
2019-10-15 10:01:31 -0400

    models-user.js

---
+++ models/user.js
@@ -0,0 +1,65 @@
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [3, 50],
+                    msg: "User first name must have at least 3 and max 50 characters"
+                }
+            }
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [3, 50],
+                    msg: "User last name must have at least 3 and max 50 characters"
+                }
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: {
+                args: true,
+                msg: "This e-mail is already registered"
+            },
+            validate: {
+                len: {
+                    args: [1, 200],
+                    msg: "E-mail must have max 200 characters"
+                }
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [8, 60],
+                    msg: "Password must have at least 8 characters"
+                }
+            }
+        }
+
+    },
+        {
+            freezeTableName: true
+        });
+
+    User.associate = function (models) {
+        User.hasMany(models.Post, {
+
+            foreignKey: {
+                allowNull: false
+            }
+        });
+
+
+    };
+
+    return User;
+};
