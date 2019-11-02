2e48437e25094b89f1d71c0004d1792d02cded9c
soheila <soheilalotfi@ymail.com>
2019-11-02 11:45:16 -0400

    add-frontend-uploading-image

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 export default {
   // Get Ads info by category
-  getAdsByCategory: function(category) {
+  getAdsByCategory: function (category) {
     return axios.get("/api/retailRoutes/" + category);
   },
 
   // Get sall ad information
-  getAllAds: function() {
+  getAllAds: function () {
     return axios.get("/api/retailRoutes");
   },
   //   // Deletes the book with the given id
@@ -16,22 +16,26 @@ export default {
   //   },
 
   //  Post or Saves a new Ad info in the database
-  saveAd: function(adData) {
+  saveAd: function (adData) {
     return axios.post("/api/retailRoutes/adpost", adData);
   },
 
   //Retrieving data for ONE Ad based on Id
-  getOneAdById: function(id) {
+  getOneAdById: function (id) {
     return axios.get("/api/retailRoutes/" + id);
   },
 
   //post or create a new user info
-  postNewUser: function(userInfo) {
-    return axios.post("/api/user/adduser",userInfo);
+  postNewUser: function (userInfo) {
+    return axios.post("/api/user/adduser", userInfo);
   },
 
   //get A user info by Email and do the authentication
-  getOneUserByEmail: function() {
+  getOneUserByEmail: function () {
     return axios.post("/api/user/auth");
+  },
+  // post an image 
+  uploadImage: function (data) {
+    return axios.post("/api/upload/", data);
   }
 };
